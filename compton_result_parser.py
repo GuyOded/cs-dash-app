cadf10413e1f8eb5a4f93506253a607013189d75
Guy Oded <guy@somemail.com>
2024-12-14 18:04:01 +0200

    Add dash

--- compton_result_parser.py
+++ compton_result_parser.py
@@ -1,6 +1,8 @@
 import argparse
 import pathlib
 import mca_output
+import plotly.graph_objects as go
+import numpy as np
 
 
 def main():
@@ -15,6 +17,9 @@ def main():
     if args.gen_csv:
         build_csv(output_file, output_data, args.normalize_time)
 
+    if args.show_graph:
+        show_graph(output_file, output_data, args.normalize_time)
+
 
 def build_csv(output_file_path: pathlib.Path, output_data: mca_output.MCAOutput, normalize_time=False):
     output_csv_path = output_file_path.resolve().parent.joinpath(f"{output_file_path.stem}.csv")
@@ -27,11 +32,25 @@ def build_csv(output_file_path: pathlib.Path, output_data: mca_output.MCAOutput,
             csv_file.write(f"{i},{count}\n")
 
 
+def show_graph(output_file_path: pathlib.Path, output_data: mca_output.MCAOutput, normalize_time=False):
+    channel_count_list = np.array(output_data.channel_count_list)
+    if normalize_time:
+        channel_count_list /= output_data.measurement_time
+
+    fig = go.Figure(
+        data=[go.Bar(y=channel_count_list)],
+        layout_title_text=f"Count Graph {output_file_path.stem}"
+    )
+    fig.show()
+
+
 def build_argparser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser("Compton Parser Utility", description="A utility to help parse and analyze results from MCA.")
     parser.add_argument("file", help="Path to an output file of MCA, or alternatively to a folder containing output files as immediate children.", type=pathlib.Path)
-    parser.add_argument("-csv", "--gen-csv", action="store_true", dest="gen_csv")
-    parser.add_argument("-dt", "--normalize-time", action="store_true", dest="normalize_time", default=False)
+    parser.add_argument("-csv", "--gen-csv", action="store_true", dest="gen_csv", help="Generates csv with columns: Channel, Count from the given output file.", default=False)
+    parser.add_argument("-dt", "--normalize-time", action="store_true", dest="normalize_time", default=False, help="If true, all output counts will be normalized by live time.")
+    parser.add_argument("-g", "--show-graph", action="store_true", dest="show_graph", default=False, help="Shows count-channel graph for a given file.")
+    parser.add_argument("-s", "--select-roi", action="store_true", dest="")
 
     return parser
 

c97ef0370640af3c40d5bfa67b404ae1cd2a633e
Guy Oded <guy@somemail.com>
2024-12-22 23:00:36 +0200

    Add analysis for all materials in the energy channel section

--- analysis_utils.py
+++ analysis_utils.py
@@ -13,16 +13,18 @@ def find_background_noise(channel_counts: np.ndarray) -> tuple[float, float]:
 
 
 def generate_model_data_from_mca_output(mca_output: MCAOutput, normalize_time=True) -> ModelData:
-    channel_count_list = np.array(mca_output.channel_count_list, dtype=np.float32)
+    channel_counts = np.array(mca_output.channel_count_list, dtype=np.float32)
+    channel_count_resolution = 1
     if normalize_time:
-        channel_count_list /= mca_output.measurement_time
+        channel_counts /= mca_output.measurement_time
+        channel_count_resolution = 1 / mca_output.measurement_time
 
-    channel_count_list = channel_count_list
+    channel_count_uncertainty = np.sqrt(np.full([len(channel_counts)], channel_count_resolution / 3) + channel_counts)
 
     channel_index = np.arange(len(mca_output.channel_count_list), dtype=np.float32)
     channel_index_uncertainty = np.full([len(channel_index)], 1/np.sqrt(3))
 
-    return ModelData(channel_index, channel_index_uncertainty, channel_count_list, np.sqrt(channel_count_list))
+    return ModelData(channel_index, channel_index_uncertainty, channel_counts, channel_count_uncertainty)
 
 def slice_model_data(model_data: ModelData, start_index: int, end_index: int):
     return ModelData(model_data.x_data[start_index:end_index], model_data.x_error[start_index:end_index], model_data.y_data[start_index:end_index], model_data.y_error[start_index:end_index])

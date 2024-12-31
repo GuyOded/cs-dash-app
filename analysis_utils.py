Fix an issue with error calculation
Add fits for klein nishina for plastic

analysis_utils.py
@@ -19,7 +19,7 @@ def generate_model_data_from_mca_output(mca_output: MCAOutput, normalize_time=Tr
         channel_counts /= mca_output.measurement_time
         channel_count_resolution = 1 / mca_output.measurement_time
 
-    channel_count_uncertainty = np.sqrt(np.full([len(channel_counts)], channel_count_resolution / 3) + channel_counts)
+    channel_count_uncertainty = np.sqrt(np.full([len(channel_counts)], channel_count_resolution / 3) + channel_counts / (mca_output.measurement_time**2))
 
     channel_index = np.arange(len(mca_output.channel_count_list), dtype=np.float32)
     channel_index_uncertainty = np.full([len(channel_index)], 1/np.sqrt(3))

curve_fitter.py
@@ -58,7 +58,7 @@ def klein_nishina_model(beta: tuple[float, float], x: float):
     electron_radius, compton_wave_length = beta
     angle_cosine = np.cos(np.deg2rad(x))
     return (electron_radius**2 / 2) * ((1 + angle_cosine**2) / (1 + compton_wave_length*(1 - angle_cosine))**2) * \
-    (1 + ((compton_wave_length*(1 - angle_cosine))**2) / ((1 + angle_cosine**2)*(1 + compton_wave_length*(1 - angle_cosine))))
+    (1 + (((compton_wave_length*(1 - angle_cosine))**2) / ((1 + angle_cosine**2)*(1 + compton_wave_length*(1 - angle_cosine)))))
 
 
 def vectorized_line(free_term: float, slope: float):

plotly_utils.py
@@ -20,8 +20,8 @@ def generate_scatter_and_line_plot(scatter: np.ndarray, *lines: list[np.ndarray]
 
     return fig
 
-def     generate_scatter_with_x_and_lines_plot(scatter: np.ndarray, scatter_x: np.ndarray, *lines: list[np.ndarray], error_y = None):
-    scatter = go.Scatter(y=scatter, x=scatter_x, mode="markers", error_y=dict(type="data", array=error_y, visible=True))
+def generate_scatter_with_x_and_lines_plot(scatter: np.ndarray, scatter_x: np.ndarray, *lines: list[np.ndarray], error_y = None, legend_name="Measurements"):
+    scatter = go.Scatter(y=scatter, x=scatter_x, mode="markers", error_y=dict(type="data", array=error_y, visible=True), name=legend_name)
     lines_graph = [go.Scatter(y=line, mode="lines") for line in lines]
     fig = go.Figure(data=[scatter] + lines_graph)
 